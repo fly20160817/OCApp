c0d013c83cd593b11b133ad788696f1ea6686702
“fly” <809176334@qq.com>
2024-04-25 01:49:59 +0800

    添加FLYNotifications类

---
+++ OCApp/Tools/FLYNotifications.h
@@ -0,0 +1,16 @@
+//
+//  FLYNotifications.h
+//  OCApp
+//
+//  Created by fly on 2024/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FLYNotifications : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCApp/Tools/FLYNotifications.m
@@ -0,0 +1,12 @@
+//
+//  FLYNotifications.m
+//  OCApp
+//
+//  Created by fly on 2024/4/25.
+//
+
+#import "FLYNotifications.h"
+
+@implementation FLYNotifications
+
+@end
