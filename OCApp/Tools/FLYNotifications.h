5374493957d79351510ac1a9a6452214866fdad7
周飞 <809176334@qq.com>
2024-04-25 19:00:58 +0800

    Update FLYNotifications.h

--- OCApp/Tools/FLYNotifications.h
+++ OCApp/Tools/FLYNotifications.h
@@ -11,6 +11,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FLYNotifications : NSObject
 
+  // 食物温度-报警通知
+UIKIT_EXTERN NSNotificationName const _Nonnull FLYFoodTemperatureAlertNotification;
+
+
+// 炉温-低温报警通知
+UIKIT_EXTERN NSNotificationName const _Nonnull FLYOvenTemperatureLowAlertNotification;
+
+// 炉温-高温报警通知
+UIKIT_EXTERN NSNotificationName const _Nonnull FLYOvenTemperatureHighAlertNotification;
+  
 @end
 
 NS_ASSUME_NONNULL_END
