464a3bdf5ff865b5cfb5863fd9225b6a276251b4
周飞 <809176334@qq.com>
2024-04-25 19:01:25 +0800

    Update FLYNotifications.m

--- OCApp/Tools/FLYNotifications.m
+++ OCApp/Tools/FLYNotifications.m
@@ -9,4 +9,14 @@
 
 @implementation FLYNotifications
 
+// 食物温度-报警通知
+NSNotificationName const FLYFoodTemperatureAlertNotification = @"FLYFoodTemperatureAlertNotification";
+
+
+// 炉温声音报警通知
+NSNotificationName const FLYOvenTemperatureLowAlertNotification = @"FLYOvenTemperatureLowAlertNotification";
+
+// 炉温振动报警通知
+NSNotificationName const FLYOvenTemperatureHighAlertNotification = @"FLYOvenTemperatureHighAlertNotification";
+
 @end
